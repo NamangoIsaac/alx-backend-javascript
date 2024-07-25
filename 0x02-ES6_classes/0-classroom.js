6c8a50ad68664d8fb81187b52a8c8272109ffc7c
Isaac_Namango <isaacnash04@gmail.com>
2024-07-25 16:07:55 +0000

    es6 classes

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,16 @@
+/*
+ *prototype for the class we are creating
+ */
+
+export default class ClassRoom {
+
+  /*
+   * constructor to take in maxStudentSize
+   * and assign it to _maxStudentSize
+   */
+
+  
+  constructor(maxStudentsSize){
+    this._maxStudentsSize = maxStudentsSize
+  }
+}
